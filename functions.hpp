48d09c5e4589b223ccb2275eb3f2ecac7b696b07
tmichael18 <70245285+tmichael18@users.noreply.github.com>
2021-02-28 07:23:02 -0500

    Add files via upload

---
+++ functions.hpp
@@ -0,0 +1,34 @@
+
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+#include <string>
+
+std::string getLastName();
+std::string getFirstName();
+
+std::string getStreetNum();
+
+//TODOS
+std::string getStreetAddress(); //returns address string from file
+std::string getCity();
+std::string getState();
+std::string getZipCode();
+
+int getInitialBalance(std::string deposit); // currently returns income
+
+std::string getDeposit();
+//int getWithdrawal(std::string deposit);
+std::string getLoans();
+double getPurchases(std::string deposit);
+
+//other supporting functions
+double getRandomPercentage();
+double getDecimal();
+double getIncomeBasedOnDeposit(std::string s_deposit);
+double getRandomNormalLinearizedVariable(double mean, double stddev);
+
+void Test();
+
+
+#endif
